1cf6d41f7d4666762d22b8a1e1c0d31387eb624d
yhha001 <yhha001@gmail.com>
2026-05-07 20:14:09 +0900

    Make RMM import lazy in clean release

--- utils/factory.py
+++ utils/factory.py
@@ -1,10 +1,10 @@
 from models.foster import FOSTER
-from models.rmm import RMM_FOSTER
 def get_model(model_name, args):
     name = model_name.lower()
     if name == "foster":
         return FOSTER(args)
     elif name == "rmm-foster":
+        from models.rmm import RMM_FOSTER
         return RMM_FOSTER(args)
     else:
         assert 0, "Not Implemented!"
